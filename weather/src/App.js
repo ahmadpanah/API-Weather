a099a957ca7c24c582b3f55f2d53b63b03061bf8
Hossein <djalicrt@gmail.com>
2022-06-01 12:28:52 +0430

    fetch method + input and search button

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [ place , setPlace] = useState("Tehran");
+
+  const handleFetch =() => {
+  fetch(`http://api.weatherapi.com/v1/current.json?key=2033d341bf9a4ca390270300220106&q=${place}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={place} onChange={(e) => setPlace(e.target.value)} />
+      <button onClick={handleFetch}>Search</button>
     </div>
   );
 }
